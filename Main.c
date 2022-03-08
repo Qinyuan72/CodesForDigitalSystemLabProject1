cdfe9dd161d1bb24aaef6f2521a2925a8ff154d8
Qinyuan Liu <fengziyuan72@gmail.com>
2022-03-08 17:52:52 +0000

    V0.6 Restructured the code and added comment

--- Main.c
+++ Main.c
@@ -1,12 +1,12 @@
 /*
- * ADC_FR_EX2.c
+ * Main.c
  *
- * ADC Example using Free Running Mode
- * VREF = AVCC - simplest setup for us to use
+ * Code for assinment 1 of Digital system
  *
- * Created: 3/11/2020 16:38:00, this program uses the background to display ADC,
+ * Created: 3/8/2022 16:38:00
  * 14/02/2022, int8_t used for flag
  * Author : ciaran.macnamee
+ * Author : Qinyuan.Liu, Mandisi.Sibanda 
  */
 
 #include <math.h>
@@ -34,9 +34,11 @@ uint8_t PORTV = 0x01;
 int derection = 0;
 
 /*
-
+    @param int    limit The upper limit for the cylon.
+    @param int twoPower    twoPower = 2^(limit-1), can't find a way to incroprate this equation in to the code, need help.
+    this function is to achive cylon efficet, called at both the full portD cylon, and the split cylon.
 */
-void move_bit(int limit,int twoPower)//twoPower = 2^(limit-1)??
+void move_bit(int limit, int twoPower)
 {
     ++move_leds_counter;
     if (derection == 0)
@@ -58,6 +60,55 @@ void move_bit(int limit,int twoPower)//twoPower = 2^(limit-1)??
         if (derection == 0)
             PORTV = twoPower; // 2^(limit-1)
         derection = ~derection;
+    }
+}
+
+/*
+    Display ADC value on o-7 bit of portD. should just directily set PortD?
+*/
+void ADC_reading_display_full_ADC()
+{
+    if (adc_reading < ONE_EIGHTH_VOLTAGE)
+        PORTD = PORTD | 0b00000000;
+    else if (adc_reading < ONE_QUARTER_VOLTAGE)
+        PORTD = PORTD | 0b00000001;
+    else if (adc_reading < THREE_EIGHTH_VOLTAGE)
+        PORTD = PORTD | 0b00000011;
+    else if (adc_reading < HALF_VOLTAGE)
+        PORTD = PORTD | 0b00000111;
+    else
+        PORTD = 0b11111111;
+}
+
+/*
+    Conbin (bitwise or "|") half cylon (portD bit 0-3) and half of the ADC reading(portD bit 4-7)
+*/
+void ADC_reading_display_half_cylon_half_ADC()
+{
+    if (adc_reading < ONE_EIGHTH_VOLTAGE)
+    {
+        //      PORTD = 0x00;
+        PORTD = PORTD | 0x00;
+    }
+    else if (adc_reading < ONE_QUARTER_VOLTAGE)
+    {
+        //     PORTD = 0x00;
+        PORTD = PORTD | 0x10;
+    }
+    else if (adc_reading < THREE_EIGHTH_VOLTAGE)
+    {
+        //    PORTD = 0x00;
+        PORTD = PORTD | 0x30;
+    }
+    else if (adc_reading < HALF_VOLTAGE)
+    {
+        // PORTD = 0x00;
+        PORTD = PORTD | 0x70;
+    }
+    else
+    {
+        // PORTD = 0x00;
+        PORTD = PORTD | 0xF0;
     }
 }
 
@@ -88,64 +139,24 @@ int main(void)
 
     while (1)
     {
-        if ((PINB & 0x20) == 0x20) ///
+        if (new_adc_data_flag)
         {
-            if (new_adc_data_flag)
+            if ((PINB & 0x20) == 0x20) ///
             {
-                if (adc_reading < ONE_EIGHTH_VOLTAGE)
-                {
-                    //      PORTD = 0x00;
-                    PORTD = PORTD | 0x00;
-                }
-                else if (adc_reading < ONE_QUARTER_VOLTAGE)
-                {
-                    //     PORTD = 0x00;
-                    PORTD = PORTD | 0x10;
-                }
-                else if (adc_reading < THREE_EIGHTH_VOLTAGE)
-                {
-                    //    PORTD = 0x00;
-                    PORTD = PORTD | 0x30;
-                }
-                else if (adc_reading < HALF_VOLTAGE)
-                {
-                    // PORTD = 0x00;
-                    PORTD = PORTD | 0x70;
-                }
-                else
-                {
-                    // PORTD = 0x00;
-                    PORTD = PORTD | 0xF0;
-                }
-
-                new_adc_data_flag = 0;
-            }
-        }
-        else
-        {
-            if ((PINB & 0x10) == 0x10)
-            {
-                if (new_adc_data_flag)
-                {
-                    if (adc_reading < ONE_EIGHTH_VOLTAGE)
-
-                        PORTD = PORTD | 0b00000000;
-                    else if (adc_reading < ONE_QUARTER_VOLTAGE)
-                        PORTD = PORTD | 0b00000001;
-                    else if (adc_reading < THREE_EIGHTH_VOLTAGE)
-                        PORTD = PORTD | 0b00000011;
-                    else if (adc_reading < HALF_VOLTAGE)
-                        PORTD = PORTD | 0b00000111;
-                    else
-                        PORTD = 0b11111111;
-
-                    new_adc_data_flag = 0;
-                }
+                ADC_reading_display_half_cylon_half_ADC();
             }
             else
             {
-                PORTD = PORTD | 0x00;
+                if ((PINB & 0x10) == 0x10)
+                {
+                    ADC_reading_display_full_ADC();
+                }
+                else
+                {
+                    PORTD = PORTD | 0x00;
+                }
             }
+            new_adc_data_flag = 0;
         }
     }
 }
@@ -158,21 +169,21 @@ ISR(TIMER0_OVF_vect)
     {
         if ((PINB & 0x20) == 0x20)
         {
-            move_bit(4,8);
+            move_bit(4, 8);
         }
         else
         {
             if ((PINB & 0x10) == 0x10)
                 PORTD = 0;
             else
-                move_bit(8,128);
+                move_bit(8, 128);
         }
         timecount0 = 0;
     }
 }
+
 ISR(ADC_vect) /* handles ADC interrupts  */
 {
-    adc_reading = ADC; /* ADC is in Free Running Mode - you don't have to set up anything for
-                          the next conversion */
+    adc_reading = ADC; /* ADC is in Free Running Mode - you don't have to set up anything for the next conversion */
     new_adc_data_flag = 1;
 }
